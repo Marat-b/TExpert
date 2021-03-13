d2105833d2f5cd0bf2973cad3013b6dd57144636
Marat-b <66325418+Marat-b@users.noreply.github.com>
2021-03-13 17:04:43 +0500

    Add files via upload

---
+++ Infrastructure/SessionEnabledControllerHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.WebHost;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+
+namespace TExp.Infrastructure
+{
+    
+        public class SessionEnabledControllerHandler : HttpControllerHandler, IRequiresSessionState
+        {
+            public SessionEnabledControllerHandler(RouteData routeData)
+                : base(routeData)
+            { }
+        }
+    
+}
